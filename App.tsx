13a861650493fa2a2c6035f1fcdf02f473b48f88
Simon Taylor <Simonwtaylor93@gmail.com>
2021-12-23 08:23:58 +0000

    Removed unused code and added example flex views

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ backgroundColor: 'green', padding: 16, borderBottomColor: 'black', borderBottomWidth: 2 }}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, flexGrow: 1, backgroundColor: 'blue', padding: 16 }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style={'auto'} />
+    </>
   );
 }
 
